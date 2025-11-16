da8f07927b0cad11bc47a92d933d88c5a2965737
Samuel W Linton <samuelwlinton@gmail.com>
2025-11-16 13:20:23 -0800

    Add distance sensor to library

--- lib/orbit/__init__.py
+++ lib/orbit/__init__.py
@@ -2,5 +2,7 @@ from .buzzer import Buzzer
 from .led import Led
 from .ble_server import BLEServer
 from .ble_client import BLEClient
+from .distance_sensor import DistanceSensor
 
-__all__=['Buzzer', 'Led', 'BLEServer', 'BLEClient']
+
+__all__=['Buzzer', 'Led', 'BLEServer', 'BLEClient', 'distance_sensor']
