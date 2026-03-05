17376011a21e4a8fecc04dfcf484606efdf69d44
Samuel W Linton <samuelwlinton@gmail.com>
2026-03-04 17:12:02 -0800

    Display faces on oled

--- sandbox/oled/oled.py
+++ sandbox/oled/oled.py
@@ -7,10 +7,11 @@
 from machine import I2C, Pin
 from ssd1306 import SSD1306_I2C
 from time import sleep
+import framebuf
 
 class Oled(SSD1306_I2C):
     WIDTH = 128
-    HEIGHT = 32
+    HEIGHT = 64
     def __init__(self, i2c_num=0, scl_pin=1, sda_pin=0) -> None:
         sleep(0.5)
         i2c = I2C(i2c_num, scl=Pin(scl_pin), sda=Pin(sda_pin), freq=200000)
@@ -28,9 +29,24 @@ class Oled(SSD1306_I2C):
         self.draw_eyes()
         self.draw_mouth()
         self.show()
+        
+    def draw_face(self, expression)-> None:
+        filename = expression + '.bin'
+        with open(filename, "rb") as f:
+            data = bytearray(f.read())
+        fb = framebuf.FrameBuffer(data, 128, 64, framebuf.MONO_HLSB)
+        oled.fill(0)
+        oled.blit(fb, 0, 0)
+        oled.show()
+    
 
 if __name__ == '__main__':
     from time import sleep
-
     oled = Oled()
-    oled.draw_neutral()
+    
+    expressions = ['happy','angry','neutral','sad','scared','sleepy','surprised']
+    while True:
+        for expression in expressions:
+            oled.draw_face(expression)
+            sleep(1)
+    
