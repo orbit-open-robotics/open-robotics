dce938b22d05c94ff0e9e058474c41177b584163
Samuel W Linton <samuelwlinton@gmail.com>
2026-03-17 18:04:53 -0700

    Update web_client.py

--- sandbox/mqtt/pico code/web_client.py
+++ sandbox/mqtt/pico code/web_client.py
@@ -2,8 +2,8 @@
 # WebClient
 # Requires micropython.umqtt.simple library
 #
-import network
-import time
+from network import WLAN, STA_IF
+from time import sleep
 from umqtt.simple import MQTTClient
 
 # MQTT Information
@@ -19,51 +19,48 @@ TOPIC_SUBSCRIBE = b"orbit_pico/commands"  # Pico listens for commands here
 
 class WebClient:
     def __init__(self, network_name: str, password: str, id: str) -> None:
-        self.network_name = network_name
-        self.password = password
-        self.id = id
+        self.id: str = id
+        self._connect_to_wifi(network_name=network_name, password=password)
+        self._connect_mqtt()
         
-    def connect_to_wifi(self) -> None:
-        wlan = network.WLAN(network.STA_IF)
+    def _connect_to_wifi(self, network_name: str, password: str) -> None:
+        wlan: WLAN = WLAN(STA_IF)
         wlan.active(True)
-        wlan.connect(self.network_name, self.password)
+        wlan.connect(network_name, password)
         print("Connecting to WiFi", end="")
         while not wlan.isconnected():
             print(".", end="")
-            time.sleep(0.5)
+            sleep(0.5)
         print("\nConnected! IP:", wlan.ifconfig()[0])
         
-    def connect_mqtt(self):
-        client_id = CLIENT_ID_BASE + self.id
-        self.client = MQTTClient(client_id, MQTT_BROKER, MQTT_PORT)
+    def _connect_mqtt(self) -> None:
+        client_id: str = CLIENT_ID_BASE + self.id
+        self.client: MQTTClient = MQTTClient(client_id, MQTT_BROKER, MQTT_PORT)
         self.client.set_callback(self.on_message)
         self.client.connect()
         self.client.subscribe(TOPIC_SUBSCRIBE)
         print(f"Connected to broker: {MQTT_BROKER}")
         print(f"Subscribed to: {TOPIC_SUBSCRIBE.decode()}")
         
-    
     def on_message(self, topic, message)-> None:
-        print(f"Received on {topic.decode()}: {msg.decode()}")
+        print(f"Received on {topic.decode()}: {message.decode()}")
         
-    def listen(self)-> None:
-        counter = 0
+    def start(self)-> None:
+        counter: int = 0
         while True:
             # Check for incoming messages (non-blocking)
             self.client.check_msg()
 
             # Publish some data every 5 seconds
-            payload = f"hello from pico, count={counter}"
+            payload: str = f"hello from pico, count={counter}"
             self.client.publish(TOPIC_PUBLISH, payload)
             print(f"Published: {payload}")
 
-            time.sleep(5)
+            sleep(5)
     
     
 if __name__ == "__main__":
     SSID = "Room32"
     PASSWORD = "password32"
-    web_client = WebClient(SSID, PASSWORD, "0")
-    web_client.connect_to_wifi()
-    web_client.connect_mqtt()
-    web_client.listen()
+    web_client: WebClient = WebClient(SSID, PASSWORD, "0")
+    web_client.start()
