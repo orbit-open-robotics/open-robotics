0586950a4a08ebbe2c3e5d4576f35a9d984f16e7
Samuel W Linton <samuelwlinton@gmail.com>
2026-04-11 16:20:39 -0700

    Add menu version of some test methods

---
+++ tests/test_drive_train_menu.py
@@ -0,0 +1,58 @@
+from orbit import Tester
+from orbit import DriveTrain
+
+def test_forward(dt: DriveTrain) -> None:
+    print('forward for 1 second...', end='')
+    dt.forward(100, 1)
+    print('done.')
+    print(repr(dt))
+    
+def test_backward(dt: DriveTrain) -> None:
+    print('Testing backward method...', end='')
+    dt.backward(100, 1)
+    dt.print_state()
+    sleep(2)
+    print('done.')
+    
+def test_stop(dt: DriveTrain) -> None:
+    print('Testing stop method...', end='')
+    dt.forward(100)
+    sleep(1)
+    dt.stop()
+    dt.print_state()
+    sleep(2)
+    print('done.')
+    
+def test_move(dt: DriveTrain) -> None:
+    print('Testing move method...', end='')
+    dt.move(100, 100)
+    dt.print_state()
+    sleep(2)
+    print('done.')
+    
+def test_swerve_left(dt: DriveTrain) -> None:
+    print('Test swerve left..', end='')
+    dt.move(75, 100)
+    dt.print_state()
+    sleep(2)
+    print('done.')
+    
+def test_swerve_right(dt: DriveTrain) -> None:
+    print('Test swerve right..', end='')
+    dt.move(100, 75)
+    dt.print_state()
+    sleep(2)
+    print('done.')
+    
+    
+
+tester = Tester()
+dt = DriveTrain()
+
+tester.add("forward", test_forward, dt)
+tester.add("backward", test_forward, dt)
+tester.add("stop", test_forward, dt)
+tester.add("move", test_forward, dt)
+tester.add("swerve left", test_forward, dt)
+tester.add("swerve right", test_forward, dt)
+tester.run_tests()
--- tests/test_led_menu.py
+++ tests/test_led_menu.py
@@ -6,13 +6,12 @@ from machine import Pin, Timer
 from time import sleep
 import uasyncio as asyncio
 from orbit import Led
+from orbit import Tester
 
 if __name__ == '__main__':
-    try:
-        led = Led(pin=6)
 
-        # Test basic on/off functionality
-        print('Test basic blink 4 times...', end='')
+    def test_blink():
+        print('Testing blink 4 times...', end='')
         for _ in range(4):
             led.on()
             sleep(0.5)
@@ -20,8 +19,8 @@ if __name__ == '__main__':
             sleep(0.5)
         print('done.')
         sleep(2)
-
-        # Test toggle functionality
+      
+    def test_toggle():
         print('Test toggle 4 times...', end='')
         for _ in range(4):
             led.toggle()
@@ -29,39 +28,35 @@ if __name__ == '__main__':
         print('done.')
         sleep(2)
         
-        # Test default blink
-        print('Test default blink, turn on for 3 seconds...', end='')
+    def test_default_blink():
+        print('Test default blink, on for 3 seconds...', end='')
         led.blink()
-        sleep(3)
+        sleep(5)
         led.off()
-        print('done.')
+        print('done')
         sleep(2)
 
-        # Test blink with custom periods
+    def test_blink_with_periods():
         print('Test blink with specified periods (50, 500)...', end='')  
         led.blink(periods=(50, 500), count = 2)
         print('done.')
         sleep(2)
-
-        # Test auto blink
-        print('Auto blink for 5 seconds...', end='')
-        led.blink()
-        sleep(5)
-        led.off()
-        print('done.')
-        sleep(2)
         
-        # Test blink with multiple periods
+    def test_blink_with_multiple_periods():
         print('Blink sequence 1, 2, 3 seconds on, repeated 2 times...', end='')
         led.blink(periods=(1000, 500, 2000, 500, 3000, 500), count=2)
         sleep(10)  # Wait for the blink sequence to complete
         print('done.')
         led.off()
         
-        print('All tests completed.')
-        
-    except KeyboardInterrupt:
-        print('Keyboard interrupt.')
-        led.off()
+    led = Led(pin=6)
+    tester = Tester()
+    
+    tester.add("blink", test_blink)
+    tester.add("toggle", test_toggle)
+    tester.add("default blink", test_default_blink)
+    tester.add("default blink with on/off periods", test_blink_with_periods)
+    tester.add("default blink with multiple on/off periods", test_blink_with_multiple_periods)
+    tester.run_tests()
         
 
