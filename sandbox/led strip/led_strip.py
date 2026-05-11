2cc78c8f60b580846a9542c3835112584ad0db4c
Sam Linton <sam.linton@harker.org>
2026-05-11 15:43:59 -0700

    Create led_strip.py

---
+++ sandbox/led strip/led_strip.py
@@ -0,0 +1,17 @@
+from machine import Pin
+from neopixel import NeoPixel
+
+class LEDStrip:
+    def __init__(self, num_leds = 64)
+        self._num_leds = num_leds
+        self._np = NeoPixel(Pin(pin), num_leds)
+        
+    def _set(self, index: int, color: tuple):
+        self._np[index] = color
+ 
+    def _show(self):
+        self._np.write()
+        
+if __name__ == '__main__':
+    led_strip = LEDStrip()
+    
